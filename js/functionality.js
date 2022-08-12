bd9b5a0b900cd99c1f27163e2e7809e6fd1796be
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-08-12 14:18:40 +0200

    funcionality continue

--- js/functionality.js
+++ js/functionality.js
@@ -10,7 +10,8 @@ addAuthorizedUser("Adam", "Nowak");
 console.log(authorizedUser);
 
 class User { 
-  constructor(name, surname, email, password) { 
+  constructor(id, name, surname, email, password) { 
+    this.id = id;
     this.name = name;
     this.surname = surname;
     this.email = email;
@@ -18,3 +19,32 @@ class User {
   }
 };
 
+const registeredUsers = {};
+
+let nextId=0;
+function createNextId() { 
+  nextId += 1;
+  return nextId;
+};
+console.log(createNextId());
+console.log(createNextId());
+console.log(nextId);
+
+const $registerForm = document.querySelector(".register-form");
+const $registerName = document.querySelector(".register-form__name-input");
+const $registerSurame = document.querySelector(".register-form__surname-input");
+const $registerEmail = document.querySelector(".register-form__email-input");
+const $registerPassword = document.querySelector(".register-form__password-input");
+  
+$registerForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const userId = createNextId();
+  const name = $registerName.value;
+  const surname = $registerSurame.value;
+  const email = $registerEmail.value;
+  const password = $registerPassword.value;
+  registeredUsers.newUser= new User(userId, name, surname, email, password);
+  return registeredUsers;
+  
+});
+console.log(registeredUsers);
